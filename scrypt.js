85fa24894eee2c28bf7d68c3dde75c62229e85fb
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-12-26 22:55:06 +0000

    fact number

---
+++ scrypt.js
@@ -0,0 +1,23 @@
+let factText = document.querySelector("#fact-p");
+let numberFactH = document.querySelector("#h3");
+let numberInput = document.querySelector("#number-input");
+numberInput.addEventListener('input', getFactFetch);
+
+
+
+function getFactFetch(){
+    let number = numberInput.value;
+
+    fetch("http://numbersapi.com/"+number)
+        .then(Response => Response.text())
+        .then(data => {
+            if(number != ''){
+                numberFactH.style.display = 'block';
+                factText.innerHTML = data;
+            }
+        })
+        .catch(err => console.log(err));
+}
+
+
+
